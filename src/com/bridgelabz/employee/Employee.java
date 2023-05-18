a70bb36e9d2b96af3a37c538db6c573a8700b634
Alok kumar prajapati <alokprajapati913@gmail.com>
2023-05-18 17:40:46 +0530

    Added:code calculate wage for a month

--- src/com/bridgelabz/employee/Employee.java
+++ src/com/bridgelabz/employee/Employee.java
@@ -3,25 +3,32 @@ package com.bridgelabz.employee;
 public class Employee {
     public static void main(String[] args) {
         System.out.println("Welcome to Wage Employee Computation Program");
-        final int FULL_TIME = 2;
-        final int WAGE_PER_HR = 20;
         final int PART_TIME = 1;
-        int empType = (int) (Math.random() * 10) % 3;
-        int workingHours = 0;
-        switch (empType) {
-                case FULL_TIME:
-                System.out.println("Employee is Present Full time");
-                workingHours = 8;
-                break;
-                case PART_TIME:
-                System.out.println("Employee is Present Part time");
-                workingHours = 4;
-                break;
-                default:
-                System.out.println("Employee is Absent");
-        }
+        final int FULL_TIME = 2;
+        final int WAGE_PER_HRS = 20;
+        final int WORKING_DAYS = 20;
+        int empHrs=0;
+        int empWage=0;
 
-                int wage = workingHours * WAGE_PER_HR;
-                System.out.println("Employee Daily Wage is " + wage);
+        int totalWage = 0;
+        for (int day = 1; day <= WORKING_DAYS; day++)
+        {
+            int empCheck = (int) (Math.random() * 10) % 3;
+            int workingHours =0;
+            switch (empCheck)
+            {
+                case FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs=0;
+            }
+            empWage= empHrs * WAGE_PER_HRS;
+            totalWage += empWage;
+        }
+        System.out.println("Total wage for a month is " + totalWage);
     }
 }
