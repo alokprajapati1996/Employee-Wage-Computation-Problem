b3775128cf6abdd0c34a05dea527eefb3b69a82b
Alok kumar prajapati <alokprajapati913@gmail.com>
2023-05-18 17:22:13 +0530

    Added:code using switch case

--- src/com/bridgelabz/employee/Employee.java
+++ src/com/bridgelabz/employee/Employee.java
@@ -3,23 +3,25 @@ package com.bridgelabz.employee;
 public class Employee {
     public static void main(String[] args) {
         System.out.println("Welcome to Wage Employee Computation Program");
-        final int PART_TIME = 1;
         final int FULL_TIME = 2;
-        final int WAGE_PER_HRS = 20;
-        int empHrs=0;
-        int empWage=0;
+        final int WAGE_PER_HR = 20;
+        final int PART_TIME = 1;
         int empType = (int) (Math.random() * 10) % 3;
-
-        if (empType == FULL_TIME) {
-            System.out.println("Employee is Present Full time");
-            empHrs = 8;
-        } else if (empType == PART_TIME) {
-            System.out.println("Employee is Present Part time");
-            empHrs = 4;
-        } else {
-            System.out.println("Employee is Absent");
-
+        int workingHours = 0;
+        switch (empType) {
+                case FULL_TIME:
+                System.out.println("Employee is Present Full time");
+                workingHours = 8;
+                break;
+                case PART_TIME:
+                System.out.println("Employee is Present Part time");
+                workingHours = 4;
+                break;
+                default:
+                System.out.println("Employee is Absent");
         }
-        empWage=empHrs*WAGE_PER_HRS;
-        System.out.println(empWage);
-    }}
+
+                int wage = workingHours * WAGE_PER_HR;
+                System.out.println("Employee Daily Wage is " + wage);
+    }
+}
