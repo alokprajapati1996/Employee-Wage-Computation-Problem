bca4dc4c6dc9b0c8f71c620fe62dfd0e1276388d
Alok kumar prajapati <alokprajapati913@gmail.com>
2023-05-18 16:36:35 +0530

    Start

---
+++ src/com/bridgelabz/employee/Employee.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.employee;
+
+public class Employee {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Wage Employee Computation Program");
+    }
+}
