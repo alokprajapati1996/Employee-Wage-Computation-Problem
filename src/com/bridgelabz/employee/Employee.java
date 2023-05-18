f077e13059a9d3c2db58fa57c9b4f71d88d60aca
Alok kumar prajapati <alokprajapati913@gmail.com>
2023-05-18 17:09:29 +0530

    Added:code add employee part time hour

--- src/com/bridgelabz/employee/Employee.java
+++ src/com/bridgelabz/employee/Employee.java
@@ -3,21 +3,23 @@ package com.bridgelabz.employee;
 public class Employee {
     public static void main(String[] args) {
         System.out.println("Welcome to Wage Employee Computation Program");
-        final int FULL_TIME = 1;
-        final int WAGE_PER_HR = 20;
-        int empType = (int) (Math.random() * 100) % 2;
-        int workingHours = 0;
-        if (empType == FULL_TIME)
-        {
-            System.out.println("Employee is Present");
-            workingHours = 8;
-        } else
-        {
+        final int PART_TIME = 1;
+        final int FULL_TIME = 2;
+        final int WAGE_PER_HRS = 20;
+        int empHrs=0;
+        int empWage=0;
+        int empType = (int) (Math.random() * 10) % 3;
+
+        if (empType == FULL_TIME) {
+            System.out.println("Employee is Present Full time");
+            empHrs = 8;
+        } else if (empType == PART_TIME) {
+            System.out.println("Employee is Present Part time");
+            empHrs = 4;
+        } else {
             System.out.println("Employee is Absent");
+
         }
-        int wage = workingHours * WAGE_PER_HR;
-        System.out.println("Employee Daily Wage is " + wage);
-    }
-
-    }
-
+        empWage=empHrs*WAGE_PER_HRS;
+        System.out.println(empWage);
+    }}
